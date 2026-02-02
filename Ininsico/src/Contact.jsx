9e8ff77ce2b41b4b9ff2f44a1d99f812c679e7ea
Ininsico <ininsico@gmail.com>
2026-02-03 02:35:07 +0500

    Deleting random things that have no fuckin useage

--- Ininsico/src/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import LoginPage from "./pages/loginpage"
-const Contact = () => {
-    return (
-        <div>
-            <LoginPage></LoginPage>
-        </div>
-    )
-}
-export default Contact
